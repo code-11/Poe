774ed0533a2f657f425e7365201ac56995e32be9
rachelboy <rachel.boy@gmail.com>
2014-02-02 16:31:13 -0500

    added conversion from IR voltage to dist

--- TestIR/TestIR.ino
+++ TestIR/TestIR.ino
@@ -3,8 +3,12 @@ int analogPin = A0;     // potentiometer wiper (middle terminal) connected to an
                        // outside leads to ground and +5V
 
 int val = 0;           // variable to store the value read
+float dist = 0;
 
 
+float getDist(int IRval) {
+  return 4590*(1.0)/((float) IRval);
+}
 
 void setup()
 
@@ -21,7 +25,10 @@ void loop()
 {
 
   val = analogRead(analogPin);    // read the input pin
+  
+  dist1 = getDist(val);
 
   Serial.println(val);             // debug value
+  Serial.println(dist);
 
 }
