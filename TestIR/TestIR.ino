7a5152fe7618caf8938d796ba4a6e9564939fa38
rachelboy <rachel.boy@gmail.com>
2014-02-03 14:36:17 -0500

    new data (with averaging)

--- TestIR/TestIR.ino
+++ TestIR/TestIR.ino
@@ -3,7 +3,9 @@
 Servo myservo;
 
 int analogPin = A0;     // potentiometer wiper (middle terminal) connected to analog pin 3
-int val = 0;           // variable to store the value read
+int val1 = 0;           // variable to store the value read
+int val2 = 0;
+int val3 = 0;
 float dist = 0;        // variable to store the calculated distance
 float pos = 0;           // variable to store the servo position
 //float tiltPos = 0;
@@ -11,6 +13,7 @@ float pos = 0;           // variable to store the servo position
 float getDist(int IRval) {
   return 4590*(1.0)/((float) IRval);
 }
+
 
 void setup()
 
@@ -29,11 +32,15 @@ void loop()
   for(pos = 0; pos<180; pos+=.5) {
     myservo.write(pos);
     delay(100);
-    val = analogRead(analogPin);    // read the input pin
-    dist = getDist(val);
+    val1 = analogRead(analogPin);    // read the input pin
+    delay(10);
+    val2 = analogRead(analogPin);
+    delay(10);
+    val3 = analogRead(analogPin);
+    dist = getDist((val1+val2+val3)/3);
     Serial.print(pos);             
     Serial.print(",");
-    Serial.print(val);
+    Serial.print((val1+val2+val3)/3);
     Serial.print(",");
     Serial.println(dist);
   }
