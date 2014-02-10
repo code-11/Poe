5beba4ec2692984148a29b6209e7c1ecb9d3bc22
Brendan Ritter <brendan.ritter@yahoo.com>
2014-02-10 14:19:41 -0500

    Included the throw out bad data code in the make nice plots script. Also committed our first time through with the custom diorama.

--- Make_nice_plots.m
+++ Make_nice_plots.m
@@ -1,3 +1,4 @@
+%Script to clean data and do coordinate transformations
 cleanData = [(data(:,1:2)./180).*pi,data(:,4)];
 cleanData(:,2) = (cleanData(:,2)-(pi/2));
 r=data(:,4);
@@ -5,6 +6,17 @@ y=r.*sin(cleanData(:,2));
 r_xy=r.*cos(cleanData(:,2));
 x = r_xy.*cos(cleanData(:,1));
 h = r_xy.*sin(cleanData(:,1));
+
+% Throw out bad data script 
+% for i = 1:length(x)
+% if ((x(i)<0) || x(i)>30)
+% c(i)=0
+% else
+% c(i)=x(i)
+% end
+% end
+
+% Do slightly more complicated coordinate transformations (Not needed)
 % Pos = zeros(size(cleanData));
 % for i = 1:length(cleanData(:,1))
 %     phi = cleanData(i,1);
